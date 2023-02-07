04d6238d51a1053dcb854a8cd23e4328e3cb962b
isaac.wadd <isaac.wadd@gmail.com>
2023-02-06 22:51:07 -0700

    Initial commit

---
+++ Models/CalculationModel.cs
@@ -0,0 +1,24 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission_5.Models {
+    public class CalculationModel {
+        [Required]
+        [Range(0, 100, ErrorMessage = "Must be between 0 and 100")]
+        public decimal grade1 { get; set; } = 0.0;
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Must be between 0 and 100")]
+        public decimal grade2 { get; set; } = 0.0;
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Must be between 0 and 100")]
+        public decimal grade3 { get; set; } = 0.0;
+
+        public decimal avgGrade { get; set; } = (self.grade1 + self.grade2 + self.grade3) / 3;
+    }
+}
